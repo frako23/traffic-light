fb02ec587897135e532641db53516740d03cdeb3
frako23 <frako23@gmail.com>
2022-09-22 14:43:40 +0000

    Se agrega el proyecto

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [ selectedColor, setSelectedColor ] = useState("rojo");
+	const [purpura,setPurpura] = useState(false);
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<><div className="caja1">
 		</div>
+		<div className={((purpura === true) ? "caja3" : "caja2")}>
+			<br />
+			<a href="#"><div 
+			onClick={() => setSelectedColor("rojo")}
+			className={"rojo"+((selectedColor === "rojo") ? " encendida" : "")}></div></a>
+			<a href="#"><div 
+			onClick={() => setSelectedColor("amarillo")}
+			className={"amarillo"+((selectedColor === "amarillo") ? " encendida" : "")}></div></a>
+			<a href="#"><div 
+			onClick={() => setSelectedColor("verde")}
+			className={"verde"+((selectedColor === "verde") ? " encendida" : "")}></div></a>
+			{purpura === true ? (
+			<a href="#"><div 
+			className="morado"></div></a>) : null}
+		</div>
+		<br />
+		<button 
+		onClick={(event) => {
+			if (selectedColor ==='rojo') {
+				setSelectedColor('amarillo')
+			} else if (selectedColor === 'amarillo') {
+				setSelectedColor('verde')				
+			} else if (selectedColor === 'verde'){
+				setSelectedColor('rojo')
+			}							
+		}}
+		type="button" class="btn btn-secondary m-5">Cambio</button>
+		<br />
+		<button 
+		onClick={() => setPurpura(true)}
+		type="button" class="btn btn-secondary mx-5">Purpura</button>
+		<br />
+		<button 
+		onClick={() => setPurpura(false)}
+		type="button" class="btn btn-secondary m-5">Purpura Reset</button>
+
+		</>
 	);
 };
 
